c3689b14bd8bed0e88da0416b54ffaee5292abf4
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-09-01 22:03:55 +0600

    url

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     # Orders
     path('orders/',include('orders.urls')),
     path('',include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
